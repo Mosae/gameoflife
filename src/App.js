ddf7737d58df5fa02b52127cbb2fce29acd1e170
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-06-23 16:48:43 -0500

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			size: [90, 20],
+		};
+		this.handleColumnChange = this.handleColumnChange.bind(this);
+		this.handleRowChange = this.handleRowChange.bind(this);
+		this.startGame = this.startGame.bind(this);
+		this.stopGame = this.stopGame.bind(this);
+		this.renderBoard = this.renderBoard.bind(this);
+	}
+	handleColumnChange(event) {}
+	handleRowChange(event) {}
+	startGame() {}
+	stopGame() {}
+	runGame() {}
+	renderBoard() {}
 
-export default App;
+	render() {
+		return (
+			<div className="worldContainer">
+				<div className="headerContainer">
+					<div className="headerInnerContaner">
+						<label className="label">
+							Rows:
+							<input
+								className="input"
+								type="text"
+								value={this.state.size[1]}
+								onChange={this.handleRowChange}
+							/>
+						</label>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
