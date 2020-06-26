76f1b4be4af2ddbba33bdf9d54ffede3132c1d24
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-06-26 14:59:25 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,183 +1,235 @@
-import React, { Component } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import Universe from './logic/Universe';
 
-export default class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			universe: new Universe(),
-			size: [20, 20],
-			gameRunning: false,
-		};
-		this.handleColumnChange = this.handleColumnChange.bind(this);
-		this.handleRowChange = this.handleRowChange.bind(this);
-		this.startGame = this.startGame.bind(this);
-		this.stopGame = this.stopGame.bind(this);
-		this.renderBoard = this.renderBoard.bind(this);
-		this.storeCell = this.storeCell.bind(this);
-	}
-	handleRowChange(event) {
-		if (!this.state.gameRunning) {
-			var actualSize = this.state.size;
-
-			if (event.target.value < 20) actualSize[1] = event.target.value;
-			else actualSize[1] = 20;
-
-			this.setState({
-				size: actualSize,
-			});
-
-			this.renderBoard();
-		}
-	}
-
-	handleColumnChange(event) {
-		if (!this.state.gameRunning) {
-			var actualSize = this.state.size;
-			if (event.target.value < 90) actualSize[0] = event.target.value;
-			else actualSize[0] = 90;
-
-			this.setState({
-				size: actualSize,
-			});
-
-			this.renderBoard();
-		}
-	}
-	startGame() {
-		if (!this.state.gameRunning) {
-			this.setState(
-				{
-					gameRunning: true,
-				},
-				() => {
-					this.intervalRef = setInterval(() => this.runGame());
-				}
-			);
-		}
-	}
-	stopGame() {
-		this.setState(
-			{
-				gameRunning: false,
-			},
-			() => {
-				if (this.intervalRef) {
-					clearInterval(this.intervalRef);
-				}
-			}
-		);
-	}
-	//this will run the logic and give us a universe for ech generation
-	runGame() {
-		this.setState({
-			universe: this.state.universe.addGeneration(),
-		});
-	}
-	storeCell(position) {
-		if (!this.state.gameRunning) {
-			this.setState({
-				universe: this.state.universe.storeCell(position),
-			});
-		}
-	}
-
-	//this will give us the playing board and also account for a cell being alive or dead
-	renderBoard() {
-		var newWorld = [];
-		var cellRow = [];
-
-		for (var i = 0; i < this.state.size[0]; i++) {
-			for (var j = 0; j < this.state.size[1]; j++) {
-				if (this.state.universe.isCellAlive(i + ' , ' + j)) {
-					cellRow.push(
-						<Cell
-							key={[i, j]}
-							position={{ x: i, y: j }}
-							live={true}
-							storeCell={this.storeCell.bind(this)}
-						/>
-					);
+function App() {
+  //instance of - check prev value
+// 	//uses .current to give the latest value
+	const canvas = useRef();
+	const currentSq = useRef();
+	const running = useRef();
+//Set the game state
+	const [square, setSquare] = useState([]);
+	const [square2, setSquare2] = useState([]);
+	const [rows, setRows] = useState(25);
+	const [columns, setColumns] = useState(25);
+	const [generation, setGeneration] = useState(0);
+	const [gameSpeed, setGameSpeed] = useState(0);
+// draw 2d canvas
+	const drawBox = (blocks) => {
+		const context = canvas.current.getContext('2d');
+//loop through to make canvas
+		for (let i = 0; i < rows; i++) {
+			for (let h = 0; h < columns; h++) {
+				if (blocks[i][h].alive) {
+					context.fillStyle = 'white';
 				} else {
-					cellRow.push(
-						<Cell
-							key={[i, j]}
-							position={{ x: i, y: j }}
-							live={false}
-							storeCell={this.storeCell.bind(this)}
-						/>
-					);
+					context.fillStyle = 'black';
+				}
+				context.fillRect(i * 10, h * 10, 10, 10);
+			}
+		}
+	};
+	useEffect(() => {
+		for (let i = 0; i < rows; i++) {
+			square.push([]);
+			square2.push([]);
+			for (let h = 0; h < columns; h++) {
+				const deadB = { alive: false };
+				square[i].push(deadB);
+				square2[i].push({ ...deadB });
+			}
+		}
+		drawBox(square2);
+		currentSq.current = '1';
+		setSquare(square);
+		setSquare2(square2);
+	}, [rows, columns]);
+
+	const click = (either) => {
+		if (running.current) {
+			return;
+    }
+    //this will draw the block onto the canvas when clicked
+		let x = Math.floor((either.clientX - either.currentTarget.offsetLeft) / 10);
+		let y = Math.floor((either.clientY - either.currentTarget.offsetTop) / 10);
+
+    //toggle on dead and alive
+		square[x][y].alive = !square[x][y].alive;
+		drawBox(square);
+	};
+
+	const gameOfLife = () => {
+		let gameBlock = square;
+    let nextBlock = square2;
+    //incorporate the rules
+		if (currentSq.current === '2') {
+			gameBlock = square2;
+			nextBlock = square;
+		}
+		for (let i = 0; i < rows; i++) {
+			for (let h = 0; h < columns; h++) {
+				let liveBlocks = 0;
+				for (let j = i - 1; j < i + 2; j++) {
+					for (let k = h - 1; k < h + 2; k++) {
+						if (j === i && k === h) {
+							continue;
+						}
+						try {
+              //if the square is alive we increase
+							if (gameBlock[j][k] && gameBlock[j][k].alive) {
+								liveBlocks++;
+							}
+						} catch (e) {
+              console.log(e)
+            }
+					}
+				}
+				if (gameBlock[i][h].alive) {
+          //if the neighbour has less than 2 or more than 3 = dead
+					if (liveBlocks < 2 || liveBlocks > 3) {
+						nextBlock[i][h].alive = false;
+					} else {
+						nextBlock[i][h].alive = true;
+					}
+				} else {
+					if (liveBlocks === 3) {
+						nextBlock[i][h].alive = true;
+					} else {
+						nextBlock[i][h].alive = false;
+					}
 				}
 			}
-			newWorld.push(
-				<div className="row" key={i}>
-					{cellRow}
-				</div>
-			);
-			cellRow = [];
 		}
+		drawBox(nextBlock);
+		if (currentSq.current === '2') {
+			currentSq.current = '1';
+		} else {
+			currentSq.current = '2';
+    }
+    //if the game is running, we are gonna animate
+		if (running.current) {
+			window.setTimeout(() => {
+				requestAnimationFrame(gameOfLife);
+			}, gameSpeed);
+		}
+		setGeneration((generation) => {
+			return generation + 1
+		});
+	};
+	const changeBoard = (e) => {
+		if (running.current) {
+			return;
+		}
+		if (e.target.name === 'rows') {
+			setRows(e.target.value);
+		} else {
+			setColumns(e.target.value);
+		}
+	};
+	const startGame = () => {
+		running.current = true;
+		requestAnimationFrame(gameOfLife);
+	};
+	const stopGame = () => {
+		running.current = false;
+	};
+	const clearBoard = () => {
+		running.current = false;
+		for (let i = 0; i < rows; i++) {
+			for (let h = 0; h < rows; h++) {
+				square[i][h].alive = false;
+				square2[i][h].alive = false;
+			}
+		}
+		drawBox(square);
+		setGeneration(0);
+	};
+	const randomizeBoard = () => {
+		for (let i = 0; i < rows; i++) {
+			for (let h = 0; h < rows; h++) {
+				if (Math.random() < 0.4) {
+					square[i][h].alive = true;
+				} else {
+					square[i][h].alive = false;
+				}
+			}
+		}
+		drawBox(square);
+		currentSq.current = '1';
+	};
+	const speed = (e) => {
+		setGameSpeed(e.target.value);
+	};
 
-		return newWorld;
-	}
-
-	render() {
-		return (
-			<div className="worldContainer">
-				<div className="headerContainer">
-					<h1>The Game Of Life</h1>
-					<div className="headerInnerContainer">
-						<label className="label">
-							Rows:
-							<input
-								className="input"
-								type="text"
-								value={this.state.size[1]}
-								onChange={this.handleRowChange}
-							/>
-						</label>
-						<label className="label">
-							Columns:
-							<input
-								className="input"
-								type="text"
-								value={this.state.size[0]}
-								onChange={this.handleColumnChange}
-							/>
-						</label>
-						{/* <label className="label">
-							Interval:
-							<input
-								className="input"
-								type="text"
-								value={this.state.interval}
-								onChange={this.changeInterval}
-							/>
-						</label> */}
+	return (
+		<div className="worldContainer">
+			<header className="headerContainer">
+				<h1>Game of Life!</h1>
+				<br />
+				<div className="headerInnerContaine">
+					<label className="label">
+						<strong>These are the rules</strong>
+						<br />
+						<li>
+							{' '}
+							Any live cell with fewer than two live neighbours dies, as if by
+							underpopulation.{' '}
+						</li>
+						<li>
+							{' '}
+							Any live cell with two or three live neighbours lives on to the
+							next generation.{' '}
+						</li>
+						<li>
+							{' '}
+							Any live cell with more than three live neighbours dies, as if by
+							overpopulation.{' '}
+						</li>
+						<li>
+							{' '}
+							Any dead cell with exactly three live neighbours becomes a live
+							cell, as if by reproduction.
+						</li>
+						
+						
+					</label>
+					<label className="board">
+						Rows:
+						<input name={'rows'} value={rows} onChange={changeBoard} />
+					</label>
+					<label className="board">
+						Columns:
+						<input name={'col'} value={columns} onChange={changeBoard} />
+					</label>
+					<div className={'Row'}>
+						<label className="label">Game Speed:</label>
+						<select className="speeds" value={gameSpeed} onChange={speed}>
+							<option value={1000}>Slow</option>
+							<option value={400}>Normal</option>
+							<option value={20}>Fast</option>
+						</select>
 					</div>
+          Generation: {generation}
 					<div className="headerButtons">
-						<button className="submit" onClick={this.startGame}>
-							Start
-						</button>
-						<button className="submit" onClick={this.stopGame}>
-							Stop
-						</button>
+						
+						<button onClick={randomizeBoard}>Randomize Board</button>
+						<button className='start' onClick={startGame}>Start</button>
+						<button className='stop' onClick={stopGame}>Stop</button>
+						<button className='reset'onClick={clearBoard}>Reset</button>
+            
 					</div>
-					Generation: {this.state.universe.getGeneration()}
+				
+					<canvas
+						onClick={click}
+						ref={canvas}
+						width={columns * 10 + 'px'}
+						height={rows * 10 + 'px'}
+					/>
 				</div>
-				<div className="boardContainer">{this.renderBoard()}</div>
-			</div>
-		);
-	}
+			</header>
+		</div>
+	);
 }
-class Cell extends Component {
-	render() {
-		return (
-			<div
-				onClick={() => this.props.storeCell(this.props.position)}
-				className={
-					this.props.live ? 'cellContainerLive' : 'cellContainerDead'
-				}></div>
-		);
-	}
-}
+
+export default App;
+
