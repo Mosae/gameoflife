bc294cd4e07d1147cab6e3b7e5a8ef2d92837896
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-09-10 19:14:29 -0500

    save changes

--- src/App.js
+++ src/App.js
@@ -51,9 +51,10 @@ function App() {
 		}
 		//this will draw the block onto the canvas when clicked
 		let x = Math.floor((either.clientX - either.currentTarget.offsetLeft) / 10);
-		let y = Math.floor((either.clientY - either.currentTarget.offsetTop) / 10);
+		let y = Math.floor((either.clientY - either.currentTarget.offsetTop) / -35);
 
 		//toggle on dead and alive
+		console.log('x and y', x, y, square);
 		square[x][y].alive = !square[x][y].alive;
 		drawBox(square);
 	};
